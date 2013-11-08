0bd31dc9af0687c0e2d05038914f14690f606b26
wenliang.shuwl <wenliang.shuwl@alibaba-inc.com>
2013-11-08 21:43:59 +0800

    添加url函数用于校验URL传参和demo文件

--- detector/detector.php
+++ detector/detector.php
@@ -6,25 +6,36 @@
            Docs - http://work.tmall.net/projects/tmall-anywhere/wiki/Check-ua
   */
 
-  $ua = $__SERVER['HTTP_USER_AGENT'];
-  $is_tablet = (bool)preg_match('#\b(ipad|tablet|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $ua );
-  $is_phone = (bool)preg_match('#\b(ip(hone|od)|android|opera m(ob|in)i|windows (phone|ce)|blackberry'.'|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.'|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $ua );
-  $device = "";
-  $is_pc = null;
+    $ua = $_SERVER['HTTP_USER_AGENT'];
+    $is_tablet = (bool)preg_match('#\b(ipad|tablet|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $ua );
+    $is_phone = (bool)preg_match('#\b(ip(hone|od)|android|opera m(ob|in)i|windows (phone|ce)|blackberry'.'|s(ymbian|eries60|amsung)|p(laybook|alm|rofile/midp|laystation portable)|nokia|fennec|htc[\-_]'.'|mobile|up\.browser|[1-4][0-9]{2}x[1-4][0-9]{2})\b#i', $ua );
+    $device = "";
+    $is_pc = null;
 
-  if($is_tablet){
-    $device = "tablet";
-    $is_pc = false;
-    $is_phone = false;
-  }else if($is_phone){
-    $device = "phone";
-    $is_pc = false;
-    $is_tablet = false;
-  }else{
-    $device = "pc";
-    $is_phone = false;
-    $is_tablet = false;
-  }
+    if($is_tablet || url("pad") ){
+        $device = "tablet";
+        $is_phone = false;
+        $is_tablet = true;
+        $is_pc = false;
+    }else if($is_phone){
+        $device = "phone";
+        $is_phone = true;
+        $is_tablet = false;
+        $is_pc = false;
+    }else{
+        $device = "pc";
+        $is_phone = false;
+        $is_tablet = false;
+        $is_pc = true;
+    }
 
-  echo "<input type=\"hidden\" value=\"$device\" id=\"J_device\" data-ua=\"$ua\" style=\"display:none;\">";
+    function url($type) {//基于URL约定的终端判断;
+        $ttid = $_GET["@ttid"];
+        $is = strpos($ttid,$type);
+        if($is !== false && $is >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 ?>
---
+++ detector/index.php
@@ -0,0 +1,6 @@
+<?php include("./detector.php") ?>
+<?php
+    echo "phone:".$is_phone."<br />";
+    echo "tablet:".$is_tablet."<br />";
+    echo "pc:".$is_pc."<br />";
+?>
